04b8b45f7338a700f255c6fff8da53da77740eaa
Gurel Ben Shabat <64422983+gurelbs@users.noreply.github.com>
2025-04-04 02:47:17 +0300

    Update udp_file_transfer.h

--- udp_file_transfer.h
+++ udp_file_transfer.h
@@ -1,61 +1,58 @@
-#ifndef UDP_FILE_TRANSFER_H
-#define UDP_FILE_TRANSFER_H
+/**
+ * Minimal UDP File Transfer System - Phase 1
+ * Header file containing shared definitions and structures
+ */
 
-#include <stdint.h>
-
-// Operation Codes (similar to TFTP with added delete)
-#define OP_RRQ     1   // Read Request
-#define OP_WRQ     2   // Write Request
-#define OP_DATA    3   // Data Packet
-#define OP_ACK     4   // Acknowledgment
-#define OP_ERROR   5   // Error
-#define OP_DELETE  6   // Delete File (custom operation)
-
-// Maximum packet size
-#define MAX_PACKET_SIZE 1024
-#define MAX_FILENAME_LENGTH 255
-#define BLOCK_SIZE 512
-
-// Error Codes
-#define ERROR_FILE_NOT_FOUND 1
-#define ERROR_ACCESS_VIOLATION 2
-#define ERROR_DISK_FULL 3
-#define ERROR_ILLEGAL_OPERATION 4
-#define ERROR_UNKNOWN_TRANSFER_ID 5
-#define ERROR_FILE_ALREADY_EXISTS 6
-#define ERROR_NO_SUCH_USER 7
-
-// Packet Structures
-// Request Packet (for RRQ, WRQ, DELETE)
-typedef struct {
-    uint16_t opcode;
-    char filename[MAX_FILENAME_LENGTH];
-    char mode[10];  // "netascii" or "octet"
-} request_packet;
-
-// Data Packet
-typedef struct {
-    uint16_t opcode;
-    uint16_t block_number;
-    uint8_t data[BLOCK_SIZE];
-} data_packet;
-
-// Acknowledgment Packet
-typedef struct {
-    uint16_t opcode;
-    uint16_t block_number;
-} ack_packet;
-
-// Error Packet
-typedef struct {
-    uint16_t opcode;
-    uint16_t error_code;
-    char error_msg[MAX_PACKET_SIZE - 4];
-} error_packet;
-
-// Function prototypes for utility functions
-int create_udp_socket();
-void set_socket_timeout(int socket, int seconds);
-void handle_error(const char* message);
-
-#endif // UDP_FILE_TRANSFER_H
+ #ifndef UDP_FILE_TRANSFER_H
+ #define UDP_FILE_TRANSFER_H
+ 
+ #include <stdint.h>
+ 
+ /* Constants */
+ #define MAX_FILENAME_LEN 64
+ #define DATA_BLOCK_SIZE 512
+ #define DEFAULT_PORT 6969
+ 
+ /* Operation Codes */
+ #define OP_RRQ   1  // Read request
+ #define OP_WRQ   2  // Write request
+ #define OP_DATA  3  // Data packet
+ #define OP_ACK   4  // Acknowledgment
+ #define OP_ERROR 5  // Error
+ 
+ /* Error Codes */
+ #define ERR_NOT_DEFINED     0
+ #define ERR_FILE_NOT_FOUND  1
+ #define ERR_ACCESS_DENIED   2
+ #define ERR_DISK_FULL       3
+ 
+ /* Packet Structures */
+ 
+ // Read/Write Request Packet
+ typedef struct {
+     uint16_t opcode;               // OP_RRQ or OP_WRQ
+     char filename[MAX_FILENAME_LEN];
+     char mode[8];                  // "octet" for binary transfer
+ } request_packet;
+ 
+ // Data Packet
+ typedef struct {
+     uint16_t opcode;           // OP_DATA
+     uint16_t block_number;
+     char data[DATA_BLOCK_SIZE];
+ } data_packet;
+ 
+ // Acknowledgment Packet
+ typedef struct {
+     uint16_t opcode;        // OP_ACK
+     uint16_t block_number;
+ } ack_packet;
+ 
+ // Error Packet
+ typedef struct {
+     uint16_t opcode;     // OP_ERROR
+     uint16_t error_code;
+     char error_msg[64];
+ } error_packet;
+ 
+ #endif /* UDP_FILE_TRANSFER_H */
